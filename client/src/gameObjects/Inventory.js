57aa22d62b2a0676ba64189af5c673274700eaae
akamagami <FelixRadermacher@gmx.de>
2021-03-27 15:59:51 +0100

    inventory

--- client/src/gameObjects/Inventory.js
+++ client/src/gameObjects/Inventory.js
@@ -7,22 +7,44 @@ export class Inventory {
     
     constructor(){
 
+        this.input.on('pointerdown', function (pointer) {
+
+            if (pointer.leftButtonDown() && pointer.rightButtonDown())
+            {
+                this.add.image(pointer.x, pointer.y, 'balls', 0);
+            }
+            else if (pointer.leftButtonDown())
+            {
+                this.add.image(pointer.x, pointer.y, 'balls', 1);
+            }
+            else if (pointer.rightButtonDown())
+            {
+                this.add.image(pointer.x, pointer.y, 'balls', 2);
+            }
+    
+        }, this);
     }
-    generateUI(scene)
+    generateUI(scene,blockMap)
     {
         this.generateBackground(scene);
+        this.generateBlockPictures(blockMap);
     }
     generateBackground(scene)
     {
         let graphics = scene.add.graphics();
-        graphics.fillStyle(0xff00ff, 1);
-        graphics.fillRoundedRect(0, 30, 70, 400, { tl: 0, tr: 22, bl: 0, br: 22 });
+        graphics.fillStyle(0xffff00, 1);
+        graphics.fillRoundedRect(0, 50, 60, 280, { tl: 0, tr: 22, bl: 0, br: 22 });
     }
-    generateBlockPictures(scene,blocks)
+    generateBlockPictures(blockMap)
     {
-        blocks.foreach((function(block){
+       blockMap.createBlock(30,100);
+       blockMap.createBlock(30,160);
+       blockMap.createBlock(30,220);
+       blockMap.createBlock(30,280);
+    }
+    clickOnBlock(blockMap)
+    {
 
-        }));
     }
     
 };
