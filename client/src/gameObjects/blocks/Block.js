8c17c8d96f8d6b6b982ac0226618c3972cd4e8c8
H0rn0chse <h0rn0chse@gmx.de>
2021-03-28 11:17:14 +0200

    Even more cleanup

--- client/src/gameObjects/blocks/Block.js
+++ client/src/gameObjects/blocks/Block.js
@@ -1,33 +1,35 @@
 const { Physics } = globalThis.Phaser;
 
 export class Block extends Physics.Arcade.Image {
-  constructor(config, sprite) {
-    super(config.scene, config.x, config.y, sprite);
-    this._isPreview = false;
-  }
+    constructor (config, sprite) {
+        super(config.scene, config.x, config.y, sprite);
+        this._isPreview = false;
+    }
 
-  onPlayerCollision(player) {
+    onPlayerCollision (player) {
 
-  }
+    }
 
-  setIsPreview(isPreview) {
-      this._isPreview = isPreview
-  }
-  isPreview() {
-      return this._isPreview;
-  }
+    setIsPreview (isPreview) {
+        this._isPreview = isPreview;
+    }
 
-  resetHighlight() {
-      this.setTint(0xffffff);
-      this.setAlpha(1);
-  }
-  setHighlightCanPlace() {
-      this.setTint(0xffffff);
-      this.setAlpha(0.7);
-  }
-  setHighlightCannotPlace() {
-      this.setTint(0xff0000);
-      this.setAlpha(0.7);
-  }
+    isPreview () {
+        return this._isPreview;
+    }
 
+    resetHighlight () {
+        this.setTint(0xffffff);
+        this.setAlpha(1);
+    }
+
+    setHighlightCanPlace () {
+        this.setTint(0xffffff);
+        this.setAlpha(0.7);
+    }
+
+    setHighlightCannotPlace () {
+        this.setTint(0xff0000);
+        this.setAlpha(0.7);
+    }
 }
--- client/src/gameObjects/blocks/BlockBoring.js
+++ client/src/gameObjects/blocks/BlockBoring.js
@@ -1,7 +1,7 @@
 import { Block } from "./Block.js";
 
 export class BlockBoring extends Block {
-    constructor(config) {
+    constructor (config) {
         super(config, "block_grass");
     }
 }
